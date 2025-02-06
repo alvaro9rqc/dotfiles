227c4ca9fec4690d167d20d144f2ba09f612e765
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-02-06 18:10:37 -0500

    Better adoption of tools

--- dependencias.sh
+++ dependencias.sh
@@ -1,79 +1,184 @@
-sudo apt install tree
-echo "Para snap"
-sudo apt install snapd
-echo "Install flatpak"
-sudo apt install flatpak
-echo "Neovim"
-sudo snap install nvim --classic
-cd ~ 
-rm .bashrc
-ln -s ~/dotfiles/bash/.bashrc .bashrc
-echo "Tmux"
-sudo snap install tmux --clasic
-cd
-ln -s ~/dotfiles/tmux/.tmux.conf .tmux.conf
-mkdir -p ~/.tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sudo apt-get install xsel # or xclip
-# leader + I en tmux para instalar
+#!/usr/bin/bash
+set -e
+
+sudo apt-get install xclip
+
+printb() {
+  echo -e "\033[1;34m"
+  echo "$1"
+  echo -e "\033[0m"
+}
+printb "Comienza la instalación :D"
+
+printb "instalar zsh?"
+read in
+if [[ "$in" == "y"  ]]; then
+  sudo apt install zsh
+  chsh -s /bin/zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  # zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  # zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  cd
+  sed -i '/export ZSH="\$HOME\/.oh-my-zsh"/a\'"source ~/dotfiles/zsh/settings.zsh" ".zshrc"
+  pluggins="
+plugins=(git 
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+)
+  "
+  sed -i 's/^plugins=(git)$/'"$pluggins"'/' ".zshrc"
+  printb "elimina el tema por defecto en zsh"
+else
+  printb "ok"
+fi
+
+
+
+printb "instalar kitty?"
+read in
+if [[ "$in" == "y"  ]]; then
+  sudo apt intall kitty \
+    && sudo update-alternatives --config x-terminal-emulator
+else
+  printb "ok"
+fi
+
+
+printb "instalar snap flatpak?"
+read in
+if [[ "$in" == "y"  ]]; then
+  sudo apt install snapd
+  sudo apt install flatpak
+else
+  printb "terminado"
+fi
+
+printb "instalar nvim y tmux?"
+read in
+if [[ "$in" == "y"  ]]; then
+  sudo snap install nvim --classic
+  sudo snap install tmux --classic
+else
+  printb "terminado"
+fi
+
+printb "instalar nodejs"
+read in
+if [[ "$in" == "y"  ]]; then
+  # Node
+  sudo apt remove libnode-dev  
+  curl -sL https://deb.nodesource.com/setup_22.x | sudo -E bash - 
+  sudo apt-get install nodejs
+  sudo npm install -g neovim
+  sudo npm install -g tree-sitter
+  sudo npm install -g tree-sitter-cli
+else
+  printb "terminado"
+fi
+
+printb "instalar pip y complementos?"
+read in
+if [[ "$in" == "y"  ]]; then
+  sudo apt install python3-pip
+  python3 -m pip install --user --upgrade pynvim
+  pip install typing-extensions #importante
+else
+  printb "terminado"
+fi
+
+printb "instalar caro y ruby?"
+read in
+if [[ "$in" == "y"  ]]; then
+  sudo apt install cargo
+  sudo apt install ruby
+else
+  printb "terminado"
+fi
+
+printb "instalar herramientas como: 
+- ripgrep, 
+- fzf
+- bat
+- git delta
+- lsd
+- colorls
+- jq
+- ctags
+- clangd
+- tree
+?"
+read in
+if [[ "$in" == "y"  ]]; then
+  sudo apt install bat
+  sudo apt-get install silversearcher-ag
+  snap install lsd
+  sudo gem install colorls
+  sudo apt install jq
+  sudo apt-get install exuberant-ctags
+  sudo apt-get install clangd
+  sudo apt install tree
+  cargo install git-delta
+  #ripgrep
+  curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb
+  sudo dpkg -i ripgrep_14.1.0-1_amd64.deb
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+else
+  printb "terminado"
+fi
+
+printb "instalar nerd font?"
+read in
+if [[ "$in" == "y"  ]]; then
+  # Nerd font
+  mkdir -p ~/.fonts
+  cd ~/.fonts
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
+  unzip JetBrainsMono.zip 
+  rm JetBrainsMono.zip 
+  fc-cache -fv
+  cd
+else
+  printb "terminado"
+fi
+
+printb "configurar enlaces simbólicos?"
+read in
+if [[ "$in" == "y"  ]]; then
+  printb "tmux"
+  ln -s ~/dotfiles/tmux/.tmux.conf .tmux.conf
+  mkdir -p ~/.tmux
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  # leader + I en tmux para instalar
+  printb "nvim"
+  cd
+  ln -s ~/dotfiles/nvim/ .config/
+  cd .config/nvim
+  mkdir autoload
+  wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  mv plug.vim autoload
+  printb "kitty"
+  ln -s ~/dotfiles/kitty/ ~/.config/
+else
+  printb "terminado"
+fi
+
+
 ###############
 # Herramientas diversas
 ###############
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb
-sudo dpkg -i ripgrep_14.1.0-1_amd64.deb
 ###############
 # Para pip
 ###############
-sudo apt install python3-pip
-pip3 install --user pynvimpara 
-pip install typing-extensions #importante
 
-# Nerd font
-mkdir -p ~/.fonts
-cd ~/.fonts
-cp ~/Descargas/JetBrainsMono.zip .
-unzip JetBrainsMono.zip 
-rm JetBrainsMono.zip 
-fc-cache -fv
-cd
 
 
 
 #Herramientas vim
-sudo apt install bat
-# noo: sudo apt install fzf
-sudo apt install xclip
-sudo apt install cargo
-cargo install git-delta
-sudo apt-get install silversearcher-ag
-sudo apt install bat #para batcat
-sudo apt install ruby
-snap install lsd
-sudo gem install colorls
 
-# Node
-sudo apt remove libnode-dev
-curl -sL https://deb.nodesource.com/setup_22.x | sudo -E bash - 
-sudo apt-get install nodejs
-npm install -g neovim
-sudo npm install -g tree-sitter
-sudo npm install -g tree-sitter-cli
-
-sudo apt install jq
-#
-#Para tag
-sudo apt-get install exuberant-ctags
-# para compulador de c
-sudo apt-get install clangd
-
-cd
-ln -s ~/dotfiles/nvim/ .config/
-cd .config/nvim
-mkdir autoload
-wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mv plug.vim autoload
-
-# intalar lo que es el diccionario para vim
+# instalar lo que es el diccionario para vim
 # dentro del directorio spell en vim
 wget --no-check-certificate https://ftp.vim.org/vim/runtime/spell/es.latin1.spl
 wget --no-check-certificate https://ftp.vim.org/vim/runtime/spell/es.latin1.sug
