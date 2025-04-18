8f5ecaa620b662a5711632aa4b75bfe68f4bcb5a
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-04-18 15:06:17 -0500

    modularization

--- nvim-lua/init.lua
+++ nvim-lua/init.lua
@@ -1,14 +1,1 @@
 require("core")
-vim.o.relativenumber = true
-vim.o.number = true
-vim.o.laststatus = 2
-vim.o.showmode = true
-vim.o.wildmode = 'list:longest,longest:full'
-vim.o.conceallevel = 2
-vim.o.spelllang = 'es'
-vim.o.guicursor = 'n-v-c:block'
-vim.o.guicursor = vim.o.guicursor .. ',a:blinkon1'
-vim.o.exrc = true
-vim.o.syntax = "enable"
-vim.cmd("colorscheme industry")
-print("Hola Mundo")
