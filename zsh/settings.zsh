0066a5c47a3ef6e7853928e9d3fea9a8d7d67bc0
alvaro9rqc <alvaro9rqc@gmail.com>
2025-10-05 18:09:25 -0500

    hmmm

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,5 +1,10 @@
-ZSH_THEME="intheloop"
 alias vim="nvim"
 alias l="lsd"
 alias graph='git log --all --graph --decorate --oneline'
+export PATH="$HOME/dotfiles/scripts:/home/alvaro9rqc/.cargo/bin:$PATH"
+export PATH="/home/alvaro9rqc/.cargo/bin:$PATH"
+plugins=(git
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+  )
 
