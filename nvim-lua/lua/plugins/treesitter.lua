2d4716fb2b74ef5459c85c881752571170be588e
alvaro9rqc <alvaro9rqc@gmail.com>
2025-12-24 22:51:15 -0500

    feat: add treesitter plugin

---
+++ nvim-lua/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+
+    priority = 100,
+  },
+}
+
