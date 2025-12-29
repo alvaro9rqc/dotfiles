0bbe53d5002f093522cebf781fd8cd5760a82703
alvaro9rqc <alvaro9rqc@gmail.com>
2025-12-28 23:12:08 -0500

    feat: icons added and global module added

--- nvim-lua/init.lua
+++ nvim-lua/init.lua
@@ -1,3 +1,15 @@
-require("core")
-require("lazyconf")
+_G.Pacha = {}
 
+Pacha.author = "Alvaro R. Quispe Condori"
+
+Pacha.icons = require("core.icons")
+
+-- Main function
+
+function main()
+  -- print(Pacha.icons.git.added)
+  require("core")
+  require("lazyconf")
+end
+
+main()
