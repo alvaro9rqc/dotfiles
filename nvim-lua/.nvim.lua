099d42247301529c364f013d030f750ed8f7bbc6
alvaro9rqc <alvaro9rqc@gmail.com>
2026-03-17 19:59:08 -0500

    chore(nvim.lua): change indent_style in formater to spaces

--- nvim-lua/.nvim.lua
+++ nvim-lua/.nvim.lua
@@ -1,6 +1,23 @@
 Pacha.setup_lsps({
   "lua_ls",
   "jsonls",
+}, {
+  lua_ls = {
+    settings = {
+      Lua = {
+        format = {
+          enable = true,
+          defaultConfig = {
+            indent_style = "space",
+            indent_size = "2",
+          },
+        },
+        diagnostics = {
+          globals = { "vim", "Pacha" },
+        },
+      },
+    },
+  },
 })
 Pacha.setup_linters({
   lua = { "luacheck" }, -- El 'key' es el filetype
