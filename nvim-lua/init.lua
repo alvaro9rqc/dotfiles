5c23899627b91082970bcfa154ddcf1b6c4101aa
alvaro9rqc <alvaro9rqc@gmail.com>
2026-05-19 23:27:49 -0500

    feat: delete old wrapper configuration

--- nvim-lua/init.lua
+++ nvim-lua/init.lua
@@ -3,12 +3,6 @@ _G.Pacha = {}
 Pacha.author = "Alvaro R. Quispe Condori"
 
 Pacha.icons = require("core.icons")
-do
-  local lsp_wrappers = require("core.lsp_wrappers")
-  Pacha.setup_lsps = lsp_wrappers.setup_lsps
-  Pacha.setup_linters = lsp_wrappers.setup_linters
-  Pacha.setup_formatters = lsp_wrappers.setup_formatters
-end
 
 -- Main function
 
