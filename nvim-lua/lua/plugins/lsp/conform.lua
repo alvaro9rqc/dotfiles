f74e32170a6921ab675716c2325176f1e011f2c5
alvaro9rqc <alvaro9rqc@gmail.com>
2026-05-19 23:32:56 -0500

    chore: formmat file with lua

--- nvim-lua/lua/plugins/lsp/conform.lua
+++ nvim-lua/lua/plugins/lsp/conform.lua
@@ -1,37 +1,35 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>cf",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			desc = "Format buffer",
-		},
-	},
-	opts = function()
-		local lsp_wrappers = require("core.lsp_wrappers")
-		return {
-			formatters_by_ft = vim.tbl_deep_extend("force", {}, lsp_wrappers.consume_pending_formatters()),
-			format_on_save = function(bufnr)
-				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-					return
-				end
-				return { timeout_ms = 3000, lsp_fallback = true }
-			end,
-		}
-	end,
-	init = function()
-		vim.g.disable_autoformat = true
-		vim.api.nvim_create_user_command("FormatToggle", function()
-			vim.g.disable_autoformat = not vim.g.disable_autoformat
-			if vim.g.disable_autoformat then
-				vim.notify("Format on save disabled", vim.log.levels.INFO)
-			else
-				vim.notify("Format on save enabled", vim.log.levels.INFO)
-			end
-		end, { desc = "Toggle format on save" })
-	end,
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    -- Tabla base vacía. Los formateadores se asignarán localmente.
+    formatters_by_ft = {},
+    format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 3000, lsp_fallback = true }
+    end,
+  },
+  init = function()
+    vim.g.disable_autoformat = true
+    vim.api.nvim_create_user_command("FormatToggle", function()
+      vim.g.disable_autoformat = not vim.g.disable_autoformat
+      if vim.g.disable_autoformat then
+        vim.notify("Format on save disabled", vim.log.levels.INFO)
+      else
+        vim.notify("Format on save enabled", vim.log.levels.INFO)
+      end
+    end, { desc = "Toggle format on save" })
+  end,
 }
