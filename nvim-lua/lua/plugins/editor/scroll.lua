2296b7039f7a3a4dadc842b77282e6857957da09
alvaro9rqc <alvaro9rqc@gmail.com>
2026-05-25 07:09:36 -0500

    feat(nvim): move scroll nvim plugin to the editor directory and improve

---
+++ nvim-lua/lua/plugins/editor/scroll.lua
@@ -0,0 +1,27 @@
+return {
+  "karb94/neoscroll.nvim",
+  event = "VeryLazy",
+  opts = {
+    mappings = {},
+    easing = "quadratic",
+    hide_cursor = true,
+  },
+  config = function(_, opts)
+    local neoscroll = require("neoscroll")
+    neoscroll.setup(opts)
+    local keymap = {
+      ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 250 }) end,
+      ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 250 }) end,
+      ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 350 }) end,
+      ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 350 }) end,
+
+      ["zt"]    = function() neoscroll.zt({ half_win_duration = 150 }) end,
+      ["zz"]    = function() neoscroll.zz({ half_win_duration = 150 }) end,
+      ["zb"]    = function() neoscroll.zb({ half_win_duration = 150 }) end,
+    }
+
+    for key, func in pairs(keymap) do
+      vim.keymap.set({ "n", "v", "x" }, key, func, { silent = true })
+    end
+  end,
+}
