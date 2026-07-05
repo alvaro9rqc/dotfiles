e2aa4bff0b9710276e9e65567fc22dccc4c483f2
alvaro9rqc <alvaro9rqc@gmail.com>
2026-07-05 06:20:31 -0500

    feat(snippets): add lsp configuration for python with uv

--- nvim-lua/lua/snippets/for_custom_files/lua.lua
+++ nvim-lua/lua/snippets/for_custom_files/lua.lua
@@ -92,4 +92,34 @@ vim.lsp.config.tinymist = {
 vim.lsp.enable("tinymist")]], "\n")),
     i(0)
   }),
+
+  -- for uv: ruff and pyright
+  nvim_lua_snippet("lsp_uv_pyright_ruff", "Configuración LSP with uv", {
+    t(vim.split([[
+local root_markers = { 'pyproject.toml', '.git' }
+local root_dir = vim.fs.root(0, root_markers)
+
+-- Identificamos la ruta exacta del intérprete que 'uv' creó en .venv
+local python_path = root_dir and (root_dir .. "/.venv/bin/python") or "python"
+
+vim.lsp.config.pyright = {
+  -- Usamos 'uv run' para invocar el langserver directamente desde tus dev-dependencies
+  cmd = { 'uv', 'run', 'pyright-langserver', '--stdio' },
+  root_markers = root_markers,
+  settings = {
+    python = {
+      pythonPath = python_path
+    }
+  }
+}
+vim.lsp.enable('pyright')
+
+vim.lsp.config.ruff = {
+  -- Invocamos el servidor LSP nativo de Ruff a través de uv
+  cmd = { 'uv', 'run', 'ruff', 'server' },
+  root_markers = root_markers,
+}
+vim.lsp.enable('ruff')]], "\n")),
+    i(0)
+  }),
 }
